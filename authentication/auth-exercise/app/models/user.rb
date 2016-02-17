be24a4ee50586a16547b3ac9f3eeb65c4dcd2fcb
Javier Garcia Jimenez <javiergarjim@Javiers-MacBook-Pro.local>
2016-02-17 11:32:12 -0500

    Updated Authentication exercise including Authorizations

--- authentication/auth-exercise/app/models/user.rb
+++ authentication/auth-exercise/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+  	unless self.node
+  		self.role = :user
+  	end
+  end
 end
